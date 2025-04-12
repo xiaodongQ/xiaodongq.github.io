8acb978730e038664b90710eddcbcae03f2231a2
xiaodongQ <xiaodong0795@163.com>
2025-04-13 07:39:29 +0800

    style: chirpy-starter

--- assets/js/define-link-target-blank.js
+++
@@ -1,4 +0,0 @@
-$(() => {
-    $('#core-wrapper .post-content a[href]:not([target],[href^="#"],.anchor,.footnote,.reversefootnote)').attr('target', '_blank')
-    $('#core-wrapper .post-content a[href][target=_blank]').append('<i class="fa fa-external-link ml-1"/>')
-})
